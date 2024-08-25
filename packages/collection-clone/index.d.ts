6a99b4330927433da593bc321894f26702f4f75e
Jeremy Scheff <jdscheff@gmail.com>
2024-08-25 10:01:53 -0400

    Update index.d.ts
    T can be any primitive too, not just an object.

--- packages/collection-clone/index.d.ts
+++ packages/collection-clone/index.d.ts
@@ -1,3 +1,3 @@
 // Definitions by: Chris Howard <https://github.com/ConnectivityChris>
-declare function clone<T extends object>(obj: T): T;
+declare function clone<T>(obj: T): T;
 export default clone;
